53bbef3142b4839a1289c18fd04222bbec61ae74
gus-aguiar <gustavopma90@gmail.com>
2022-09-22 12:17:54 -0300

    xadrez

--- fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/script.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/script.js
@@ -1,46 +1,54 @@
-const a = 5;
-const b = 6;
-const c = 7;
+// const a = 5;
+// const b = 6;
+// const c = 7;
 
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log (a / b);
-console.log(a % b);
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log (a / b);
+// console.log(a % b);
 
-if (a > b){
-    console.log("a é maior");
-} else if (a < b) { 
-    console.log("b é maior");
-} else if (a = b){
-    console.log();
-}
+// if (a > b){
+//     console.log("a é maior");
+// } else if (a < b) { 
+//     console.log("b é maior");
+// } else if (a = b){
+//     console.log();
+// }
 
-if(a > b && a > c){
-console.log('a é maior do que b e c');
-}
-else if(b > a && b > c){
-    console.log('b é maior do que a e c');
-    }
-else if(c > a && c > b){
-    console.log('c é maior do que a e b');
-} 
-if (a > 0){
-    console.log('a é positivo');
-}
-else if (a < 0){
-    console.log('a é negativo');
-}   
-else if (a = 0){
-    console.log('a é igual a zero');
-}
+// if(a > b && a > c){
+// console.log('a é maior do que b e c');
+// }
+// else if(b > a && b > c){
+//     console.log('b é maior do que a e c');
+//     }
+// else if(c > a && c > b){
+//     console.log('c é maior do que a e b');
+// } 
+// if (a > 0){
+//     console.log('a é positivo');
+// }
+// else if (a < 0){
+//     console.log('a é negativo');
+// }   
+// else if (a = 0){
+//     console.log('a é igual a zero');
+// }
 
-let angulo1 = 30;
-let angulo2 = 50;
-let angulo3 = 100;
-if(angulo1 + angulo2 + angulo3 === 180){
-console.log(true);
-} 
-else { 
-console.log(false); 
-}
+// let angulo1 = 30;
+// let angulo2 = 50;
+// let angulo3 = 100;
+// if(angulo1 + angulo2 + angulo3 === 180){
+// console.log(true);
+// } 
+// else { 
+// console.log(false); 
+// }
+
+let xadrez = '';
+if(xadrez.toLowerCase() = 'dama'){ console.log('em linha e diagonais')}
+if(xadrez.toLowerCase() = 'bispo'){ console.log('em linha e diagonais')}
+if(xadrez.toLowerCase() = 'torre'){ console.log('em linha e diagonais')}
+if(xadrez.toLowerCase() = 'cavalo'){ console.log('em linha e diagonais')}
+if(xadrez.toLowerCase() = 'peão'){ console.log('em linha e diagonais')}
+if(xadrez.toLowerCase() = 'rei'){ console.log('em linha e diagonais')}
