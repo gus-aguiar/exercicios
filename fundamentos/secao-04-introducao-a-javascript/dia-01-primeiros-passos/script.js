05542eca514918466273b82e1b080a91cd1fe5e9
gus-aguiar <gustavopma90@gmail.com>
2022-09-22 15:16:07 -0300

    fiz os exercicios propostos para o primeiro dia

--- fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/script.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/script.js
@@ -45,10 +45,83 @@
 // console.log(false); 
 // }
 
-let xadrez = '';
-if(xadrez.toLowerCase() = 'dama'){ console.log('em linha e diagonais')}
-if(xadrez.toLowerCase() = 'bispo'){ console.log('em linha e diagonais')}
-if(xadrez.toLowerCase() = 'torre'){ console.log('em linha e diagonais')}
-if(xadrez.toLowerCase() = 'cavalo'){ console.log('em linha e diagonais')}
-if(xadrez.toLowerCase() = 'peão'){ console.log('em linha e diagonais')}
-if(xadrez.toLowerCase() = 'rei'){ console.log('em linha e diagonais')}
+// let xadrez = 'bisPo';
+// switch (xadrez.toLowerCase () ){
+// case 'dama':
+//     console.log('em linha e diagonais');
+//     break;
+// case 'bispo': 
+//     console.log('diagonais')
+//     break;
+// case 'torre': 
+//     console.log('em linha')
+//     break;
+// case 'cavalo': 
+//     console.log('em L')
+//     break;
+// case 'peão': 
+//     console.log('uma casa reto')
+//     break;
+// case 'rei': 
+//     console.log('uma casa para todos os lados')
+//     break
+//     default:
+//         console.log('valor inválido')
+// }
+
+//let const1 = 7
+//let const2 = 3
+//let const3 = 9 
+
+// if(const1 % 2 === 0 ||const2 % 2 === 0 ||const3 % 2 === 0 ){
+//     console.log(true)
+// } else{console.log(false)}
+
+// let const1 = 2
+// let const2 = 4
+// let const3 = 6 
+
+// if(const1 % 2 === 1 ||const2 % 2 === 1 ||const3 % 2 === 1 ){
+//     console.log(true)
+// } else{console.log(false)}
+
+let salarioBruto = 17000
+let aliquotaINSS = 0
+let salarioLiquido = 0
+let IRSS = 0 
+
+if (salarioBruto <= 1556.94){
+     aliquotaINSS = (salarioBruto * 8) / 100
+} 
+else if (salarioBruto > 1556.94 && salarioBruto <= 2594.92){
+    aliquotaINSS = (salarioBruto * 9) / 100
+} 
+else if (salarioBruto > 2594.92 && salarioBruto <= 5189.82){
+    aliquotaINSS = (salarioBruto * 11) / 100
+} 
+else if (salarioBruto > 5198.82){
+    aliquotaINSS = 570.88
+}
+salarioDescontado1 = salarioBruto - aliquotaINSS
+if ( salarioDescontado1 < 1903.98){
+    salarioLiquido = salarioDescontado1
+}
+else if (salarioDescontado1 >= 1903.99 && salarioDescontado1 <= 2826.65){
+    IRSS = (7.5 * salarioDescontado1/100) - 142.80
+    salarioLiquido = salarioDescontado1 - IRSS 
+}
+else if (salarioDescontado1 >= 2826.66 && salarioDescontado1 <= 3751.05){
+    IRSS = (15 * salarioDescontado1/100) - 354.80
+    salarioLiquido = salarioDescontado1 - IRSS 
+}
+else if (salarioDescontado1 >= 3751.06 && salarioDescontado1 <= 4664.68){
+    IRSS = (15 * salarioDescontado1/100) - 636.13
+    salarioLiquido = salarioDescontado1 - IRSS 
+}
+else if (salarioDescontado1 > 4664.68){
+    IRSS = (27 * salarioDescontado1/100) - 869.36
+    salarioLiquido = salarioDescontado1 - IRSS 
+}
+console.log (salarioLiquido)
+
+//Tirei os let de dentro dos IFs, perguntar por que não deu pra deixar com let dentro !!!!!
