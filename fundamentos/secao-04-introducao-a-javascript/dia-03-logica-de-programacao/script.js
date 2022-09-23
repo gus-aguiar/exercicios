fc0a24b33d9d141d04f51177f65276a1af30517c
gus-aguiar <gustavopma90@gmail.com>
2022-09-23 19:22:12 -0300

    fiz o primeiro e o segundo exercicios na monitoria

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-03-logica-de-programacao/script.js
@@ -0,0 +1,19 @@
+// let fatorial = 1;
+// let vamo = 5;
+// for(let index = 1;index <= vamo; index += 1){
+//     fatorial = index * fatorial;
+// }
+// console.log(fatorial)
+
+//=========================================================
+
+// let word = 'tryber';
+// let reverso = [];
+
+// for (index = word.length - 1; index >=0 ;index-=1){
+//     reverso.push(word[index]);
+// }
+
+// console.log(reverso)
+
+//=========================================================
