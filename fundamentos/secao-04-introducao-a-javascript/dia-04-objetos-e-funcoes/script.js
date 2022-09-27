1ca8bac0b6a1ae5e4263960baa4488f51b16bdf9
gus-aguiar <gustavopma90@gmail.com>
2022-09-27 08:44:10 -0300

    fiz o exercicio 3 com ajuda do gabarito, mas esse eu entendi melhor

--- fundamentos/secao-04-introducao-a-javascript/dia-04-objetos-e-funcoes/script.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-objetos-e-funcoes/script.js
@@ -51,3 +51,59 @@
 //   )
 // //  console.log('o livro favorito de ' + leitor.nome +' '+ leitor.sobrenome +' se chama '+ leitor.livrosFavoritos[1].titulo + '.')
 // console.log(leitor.nome + " tem " + leitor.livrosFavoritos.length + " livros favoritos")
+
+//============================================começando exercícios de funções=================//
+// let palavra = 'arara'
+// function verificaPalindrome(word){
+//     for (index in word){
+//         if(word[index] != word[(word.length -1) - index]){
+//         return false;}
+//     }
+// }
+// return true; 
+// console.log(palavra.split(''))
+// console.log(palavra.reverse)
+// console.log(palavra.reverse())
+//======dúvida aqui, ver como funciona split e reverse e como o código se comportou no gabarito=============//
+
+
+// function valorMaior(numeros){
+//     let indexMaior = 0;
+//     for(let index in numeros){
+//         if(numeros[indexMaior] < numeros[index]){
+//             indexMaior = index;
+//         }
+//     }
+//     return indexMaior;
+// }
+// console.log(valorMaior([3, 15 , 777]))
+
+
+//========================copiei do gabarito===========================//
+// function indiceDoMaior(numeros) {
+//     let indiceMaior = 0;
+//     for (let indice in numeros) {
+//       if (numeros[indiceMaior] < numeros[indice]) {
+//         indiceMaior = indice;
+//       }
+//     }
+  
+//     return indiceMaior;
+//   }
+  
+//   console.log(indiceDoMaior([2, 3, 6, 7, 10, 1]));
+
+//=====================fazendo exercício 3 =========================//
+
+// function menorValor(numero){
+//   let indiceMenor = 0;
+//   for (let index in numero){
+//     if (numero[indiceMenor] > numero[index] ){
+//       indiceMenor = index;
+//     }
+//   }
+//   return indiceMenor;
+// }
+// console.log(menorValor([2, 4, 6, 7, 10, 0, -3]))
+
+//=====================exercício 04===========================//
