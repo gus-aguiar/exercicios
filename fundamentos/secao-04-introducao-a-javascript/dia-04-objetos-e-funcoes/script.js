f72f646625ec8cc3810054cff261eb9f63f9b3df
gus-aguiar <gustavopma90@gmail.com>
2022-09-27 06:49:42 -0300

    todos os exercícios de objetos e for/in feitos, vamos aos de funções

--- fundamentos/secao-04-introducao-a-javascript/dia-04-objetos-e-funcoes/script.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-objetos-e-funcoes/script.js
@@ -1,28 +1,53 @@
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
-//   console.log('Bem-vinda '+info.personagem)
-info.recorrente = 'Sim'
-// console.log(info)
-// for(let key in info){
-//     console.log(key)
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   };
+// //   console.log('Bem-vinda '+info.personagem)
+// info.recorrente = 'Sim'
+// // console.log(info)
+// // for(let key in info){
+// //     console.log(key)
+// // }
+// let infoDois = {
+//     personagem: 'Tio Patinhas’',
+//     origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+//     nota: 'O último MacPatinhas’',
+//     recorrente: 'Sim'
 // }
-let infoDois = {
-    personagem: 'Tio Patinhas’',
-    origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
-    nota: 'O último MacPatinhas’',
-    recorrente: 'Sim'
-}
-for (let key in info,infoDois){
-    if( key === "recorrente" &&
-    info[key] ==="Sim" &&
-    infoDois[key] ==="Sim"){
-        return('Ambos recorrentes')
+// for (let key in info,infoDois){
+//     if( key === "recorrente" &&
+//     info[key] ==="Sim" &&
+//     infoDois[key] ==="Sim"){
+//         return('Ambos recorrentes')
     
-}
-}
-console.log(info +" e "+ infoDois)
-
+// }
+// }
+// console.log(info +" e "+ infoDois)
+//
 //==============================dúvida aqui em cima======================================//
+
+// let leitor = {
+//     nome: 'Julia',
+//     sobrenome: 'Pessoa',
+//     idade: 21,
+//     livrosFavoritos: [
+//       {
+//         titulo: 'O Pior Dia de Todos',
+//         autor: 'Daniela Kopsch',
+//         editora: 'Tordesilhas',
+//       },
+
+//     ],
+    
+//   };
+
+//   leitor.livrosFavoritos.push(
+//     {
+//         titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+//         autor: 'JK Rowling',
+//         editora: 'Rocco',
+//       }
+//   )
+// //  console.log('o livro favorito de ' + leitor.nome +' '+ leitor.sobrenome +' se chama '+ leitor.livrosFavoritos[1].titulo + '.')
+// console.log(leitor.nome + " tem " + leitor.livrosFavoritos.length + " livros favoritos")
