ff3da13383fa100b1ce4708b7d3aca8e6fb17581
gus-aguiar <gustavopma90@gmail.com>
2022-09-30 19:32:42 -0300

    fiz o exercicio um e dois

---
+++ fundamentos/secao-05-javascript-dom-eventos-e-webstorage/dia-03-eventos/script.js
@@ -0,0 +1,41 @@
+function createDaysOfTheWeek() {
+    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+    const weekDaysList = document.querySelector('.week-days');
+  
+    for (let index = 0; index < weekDays.length; index += 1) {
+      const days = weekDays[index];
+      const dayListItem = document.createElement('li');
+      dayListItem.innerHTML = days;
+  
+      weekDaysList.appendChild(dayListItem);
+    };
+  }
+  
+  createDaysOfTheWeek();
+  
+  // Escreva seu código abaixo.
+  const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  let ulPai = document.getElementById('days')
+  for (let index in decemberDaysList){
+    let criaLi = document.createElement('li')
+    criaLi.innerHTML = decemberDaysList[index]
+    criaLi.className = 'day'
+    ulPai.appendChild(criaLi)
+  }
+  document.getElementsByClassName('day')[25].className ='day holiday'
+  document.getElementsByClassName('day')[26].className ='day holiday'
+  document.getElementsByClassName('day')[32].className ='day holiday'
+  document.getElementsByClassName('day')[5].className ='day friday'
+  document.getElementsByClassName('day')[12].className ='day friday'
+  document.getElementsByClassName('day')[19].className ='day friday'
+  document.getElementsByClassName('day')[26].className ='day friday'
+
+  function createButton (param){
+    let divPai = document.getElementsByClassName('buttons-container')[0]
+    let button = document.createElement('button')
+    button.innerText = param
+    button.id = 'btn-holiday'
+    divPai.appendChild(button)
+    
+  }
+  createButton ('feriado')
