45a3305063ec08a4e5de5d9b1835e8fc9dc4503a
gus-aguiar <gustavopma90@gmail.com>
2022-10-07 19:06:05 -0300

    cheguei em um resultado MAIS OU MENOS satisfatório
    Signed-off-by: gus-aguiar <gustavopma90@gmail.com>

--- fundamentos/secao-06-html-e-css-forms-flexbox-responsivo/dia-02-bibliotecas-javascript-e-frameworks-css/script.js
+++ fundamentos/secao-06-html-e-css-forms-flexbox-responsivo/dia-02-bibliotecas-javascript-e-frameworks-css/script.js
@@ -3,9 +3,9 @@ button.addEventListener('click', function(event){
     event.preventDefault()
 })
 
-let section = document.getElementById('section')
-let clear = document.createElement('button')
-clear.id = "clear"
-clear.innerHTML = 'limpar'
-clear.type = "reset"
-section.appendChild(clear)
+// let section = document.getElementById('section')
+// let clear = document.createElement('button')
+// clear.id = "clear"
+// clear.innerHTML = 'limpar'
+// clear.type = "reset"
+// section.appendChild(clear)
