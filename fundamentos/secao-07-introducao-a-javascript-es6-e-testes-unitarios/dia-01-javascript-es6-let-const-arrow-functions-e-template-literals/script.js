dab16893fa3827dd9d75b7413e671310a79dbe52
gus-aguiar <gustavopma90@gmail.com>
2022-10-17 18:54:21 -0300

    amanha eu continuo

--- fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -1,12 +1,21 @@
-let testingScope = (escopo) => {
-    if (escopo === true) {
-      let ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
-      ifScope = `${ifScope} +  ótimo, fui utilizada no escopo !`;
-      console.log(ifScope);
-    } else {
-      const elseScope = `Não devo ser utilizada fora do meu escopo (else)`;
-      console.log(elseScope);
-    }
-  }
+// let testingScope = (escopo) => {
+//     if (escopo === true) {
+//       let ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
+//       ifScope = `${ifScope} +  ótimo, fui utilizada no escopo !`;
+//       console.log(ifScope);
+//     } else {
+//       const elseScope = `Não devo ser utilizada fora do meu escopo (else)`;
+//       console.log(elseScope);
+//     }
+//   }
 
-  testingScope(false);
+//   testingScope(false);
+//===========exercicio 01 acima, abaixo 02 =====================//
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sortOddsAndEvens = () => {
+
+// Seu código aqui.
+
+console.log(oddsAndEvens); // será necessário alterar essa linha 😉
