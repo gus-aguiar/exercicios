28814d1eee25d2dfc2fe2a94a621b8bcaec9d519
gus-aguiar <gustavopma90@gmail.com>
2022-10-24 19:25:17 -0300

    deixei o resto pra amanhã

---
+++ fundamentos/secao-08-high-order-functions-do-JavaScript-ES-6/dia-01-JavaScript-ES6-Introducao-a-higher-order-functions/script3.js
@@ -0,0 +1,18 @@
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+let corretorExame = (RIGHT_ANSWERS,STUDENT_ANSWERS,teste()) => {
+    return `Sua pontuação foi de ${teste}`;
+}
+function teste(RIGHT_ANSWERS, STUDENT_ANSWERS) {
+    let count = 0;
+    for (let index = 1; index < RIGHT_ANSWERS.length; index += 1) { 
+        if(RIGHT_ANSWERS[index] === STUDENT_ANSWERS ){
+            count += 1
+        } else if(RIGHT_ANSWERS[index] !== STUDENT_ANSWERS ){
+            count -= 0.5
+        } else if (STUDENT_ANSWERS === N.A)
+            count += 0
+    }
+    return count
+}
