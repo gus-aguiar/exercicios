b6c23adfda7a49b52a699a50f01bc752a2bbcfcb
gus-aguiar <gustavopma90@gmail.com>
2022-10-28 18:59:46 -0300

    fiz a quatro

--- fundamentos/secao-08-high-order-functions-do-JavaScript-ES-6/dia-05-JavaScript-ES6-spread-operator-parametro-rest-destructuring-e-mais/exercise3.js
+++ fundamentos/secao-08-high-order-functions-do-JavaScript-ES-6/dia-05-JavaScript-ES6-spread-operator-parametro-rest-destructuring-e-mais/exercise3.js
@@ -14,7 +14,7 @@ const alex = {
   // name: nome da pessoa, age: idade, likes: gosta de, nationality: nacionalidade
   
   // complete a assinatura da função abaixo
-  const personLikes = () => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
+  const personLikes = ({name , age , likes}) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
   // <nome> tem <anos de idade> e gosta de <gostos da pessoa>
   
   // Retornos esperados:
