311eae5b9ee98a55dd9403e2fe96765d49e16369
gus-aguiar <gustavopma90@gmail.com>
2022-11-09 10:31:16 -0300

    testando erro

--- front-end/secao-01-Introducao-ao-Frontend-e-Javascript-assincrono/dia-01-Ambiente-e-desenvolvimento/src/main.js
+++ front-end/secao-01-Introducao-ao-Frontend-e-Javascript-assincrono/dia-01-Ambiente-e-desenvolvimento/src/main.js
@@ -1,4 +1,14 @@
 import validator from 'validator';
-document.addEventListener
 const submit = document.getElementById('submit')
-submit.addEventListener('click' validator.isAlpha)
+const texto = document.getElementById('text').value
+submit.addEventListener('click', (event) =>{event.preventDefault()
+    const campos = {
+        cpf: validator.isTaxID(texto.value, 'pt-BR'),
+        hexColor: validator.isHexColor(texto.value),
+        email: validator.isEmail(texto.value),
+        uuid: validator.isUUID(texto.value, 4),
+        url: validator.isURL(texto.value),
+      };
+      textoDeSaida.innerHTML = `A validação retornou ${campos[seletor.value]}`;
+
+})
